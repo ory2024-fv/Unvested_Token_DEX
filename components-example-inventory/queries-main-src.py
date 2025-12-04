011b01366ad1e2e4598db32bafb2db3fcb155534
ory2024-fv <aldova383267728@rambler.ru>
2025-12-05 05:15:29 +0800

    add vite.config.ts, queries-main-src.py, export.rs

---
+++ components-example-inventory/queries-main-src.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\components-example-inventory\queries-main-src.py
+// Generated at: 2025-10-20T08:16:07.645Z
---
+++ export.rs
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\export.rs
+// Generated at: 2025-10-20T08:16:07.638Z
