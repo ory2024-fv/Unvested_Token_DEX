588272e5c63a8b5d80d07086c613ddf16b206cb6
ory2024-fv <aldova383267728@rambler.ru>
2025-12-05 05:15:42 +0800

    add endpoint-responsive.go, package-lock.json, index.html

---
+++ frontend/web/src/config-spec-resolver/dist.rst
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\config-spec-resolver\dist.rst
+// Generated at: 2025-10-20T08:16:07.654Z
---
+++ frontend/web/src/customer/src-demo/product/database-decorator.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\customer\src-demo\product\database-decorator.java
+// Generated at: 2025-10-20T08:16:07.650Z
